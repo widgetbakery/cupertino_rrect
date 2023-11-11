5251c3b9246fa47f23d43fbe8b1237888befbd79
Matej Knopp <matej.knopp@gmail.com>
2023-11-12 00:11:47 +0100

    feat: add example

---
+++ example/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:cupertino_rrect/cupertino_rrect.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  double radius = 50;
+  bool showStandard = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Radius: ${radius.roundToDouble()}'),
+                  Slider(
+                    value: radius,
+                    min: 0,
+                    max: 100,
+                    onChanged: (value) {
+                      setState(() {
+                        radius = value;
+                      });
+                    },
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: showStandard,
+                        onChanged: (v) {
+                          setState(() {
+                            showStandard = v!;
+                          });
+                        },
+                      ),
+                      const Text('Show standard'),
+                    ],
+                  ),
+                ],
+              ),
+              Stack(
+                fit: StackFit.passthrough,
+                children: [
+                  if (showStandard)
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(radius),
+                        color: Colors.red,
+                      ),
+                      width: 300,
+                      height: 300,
+                    ),
+                  Container(
+                    decoration: ShapeDecoration(
+                      shape: CupertinoRectangleBorder(
+                        borderRadius: BorderRadius.circular(radius),
+                      ),
+                      color: Colors.blue,
+                    ),
+                    width: 300,
+                    height: 300,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
